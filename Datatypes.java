0b5ab472b014290226e3086e73858330e08a98ae
Harshita Choudhary <210harshita@gmail.com>
2026-01-31 17:16:01 +0530

    Datatypes

---
+++ Datatypes.java
@@ -0,0 +1,13 @@
+public class Datatypes {
+    public static void main(String args[]){
+        int num1 = 2;
+        byte by = 20;
+        short sh = 202;
+        long lg = 2021l;
+        float f = 2.1f;
+        double d = 20.21;
+        boolean b = true;
+        char c = 'k';
+    }
+    
+}
