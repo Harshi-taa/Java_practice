bbe6647f8247cb26186c59ca2fb1d5718f126ba5
Harshita Choudhary <210harshita@gmail.com>
2026-01-31 17:11:47 +0530

    Adding 2 number

---
+++ Num.java
@@ -0,0 +1,9 @@
+public class Num {
+    public static void main(String args[]){
+        int num1 = 21;
+        int num2 = 20;
+        int result = num1 +num2;
+        System.out.println(result);
+    }
+    
+}
